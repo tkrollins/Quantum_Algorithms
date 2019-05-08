b193781751a18ccb830223523535f66786f5cf81
Zach Harris <zharris505@gmail.com>
2019-05-07 18:29:34 -0700

    testing other stuff

--- DJ-BV/Bernstein-Vazirani.py
+++ DJ-BV/Bernstein-Vazirani.py
@@ -9,6 +9,9 @@ from pyquil.api import local_qvm
 
 qvm = get_qc('9q-square-qvm')
 
+p = Program()
+ro = p.declare('ro', 'BIT', 3)
+
 # initializing the bits
 state_prep = Program(I(0), I(1), I(2), X(3))
 
@@ -19,36 +22,31 @@ hadamards_left = Program(H(0), H(1), H(2), H(3))
 U_f = Program(CNOT(1, 3))
 
 # add last hadamards
-hadamards_right = Program(H(0), H(1), H(2))
+hadamards_right = Program(H(0), H(1), H(2), I(3))
 
-wf_sim = WavefunctionSimulator()
-wavefunction = wf_sim.wavefunction(state_prep + hadamards_left + U_f + hadamards_right)
-print(wavefunction)
+# measure
+measure = Program(MEASURE(0, ro[0]), MEASURE(1, ro[1]), MEASURE(2, ro[2]))
 
-# # one way of declaring program:
-# p = Program()
-# ro = p.declare('ro', 'BIT', 3)
-# p += H(0)
-# p += H(1)
-# p += H(2)
-# p += X(3)
-# p += H(3)
-# p += CNOT(1, 3)
-# p += MEASURE(0, ro[0])
-# p += MEASURE(1, ro[1])
-# p += MEASURE(2, ro[2])
-#
-# print(p)
-# # multiple trials
-# p.wrap_in_numshots_loop(5)
-#
-# with local_qvm():
-#     # one way of measuring:
-#     executable = qvm.compile(p)
-#     result = qvm.run(executable)
-#     print(result)
-#
-#     # another way of measuring:
-# #     results = qvm.run_and_measure(p, trials=10)
-# #     print(results[0])
-# #     print(results[1])
+# one way of declaring program:
+p += state_prep
+p += hadamards_left
+p += U_f
+p += hadamards_right
+p += MEASURE(0, ro[0])
+p += MEASURE(1, ro[1])
+p += MEASURE(2, ro[2])
+
+print(p)
+# multiple trials
+p.wrap_in_numshots_loop(5)
+
+with local_qvm():
+    # one way of measuring:
+    executable = qvm.compile(p)
+    result = qvm.run(executable)
+    print(result)
+
+    # another way of measuring:
+#     results = qvm.run_and_measure(p, trials=10)
+#     print(results[0])
+#     print(results[1])
