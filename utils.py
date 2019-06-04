7b2c3c83a192adb7b42108fd545dc35ef3b1df90
tkrollins <tkrollins@ucla.com>
2019-06-03 20:02:35 -0700

    Added to utils.py. DJ results notebook.

--- utils.py
+++ utils.py
@@ -1,8 +1,40 @@
-from numpy import array
+import numpy as np
 
 
 def getResultsArray(resultsDict, n):
+    """
+    Turns dict returned from QPU server into matrix.
+    :param resultsDict: results dict from QPU server
+    :param n: qubits measured
+    :return: matrix with results. Each row is a single run.
+    """
     qubits = [0, 1, 2, 7, 14, 15]
     qubits = qubits[:n]
-    results = array([resultsDict[i] for i in qubits])
+    results = np.array([resultsDict[i] for i in qubits])
     return results.transpose()
+
+def compareResults(qvm, qpu):
+    """
+    Compares results between QVM and QPU runs.
+    :param qvm: matrix of qvm results
+    :param qpu: matrix of qpu results
+    :return: fraction of equality
+    """
+    comp = np.equal(qvm, qpu)
+    row_comp = np.array([row.all() for row in comp])
+    row_comp_int = row_comp.astype(int)
+    frac_equal = row_comp_int.sum() / len(row_comp_int)
+    return frac_equal
+
+def DJ_resultCorrectness(qpu_results):
+    """
+    Determines fraction of correct results from QPU for DJ.
+    Correctness is any row that has at least a single 1.
+    :param qpu_results: matrix of qpu results
+    :return: fraction of correctness
+    """
+    correctness = qpu_results.astype(bool)
+    correctness = np.array([row.any() for row in correctness])
+    correctness = correctness.astype(int)
+    frac_correct = correctness.sum() / len(correctness)
+    return frac_correct
