9b02b1b78039c9d0826a21815e8fd472f69660d3
tkrollins <tkrollins@ucla.com>
2019-06-06 09:21:56 -0700

    "Updated Driver.cs for DJ"

--- Q#/DJ-BV/Driver.cs
+++ Q#/DJ-BV/Driver.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
@@ -9,17 +11,28 @@ namespace DJ_BV
     {
         static void Main(string[] args)
         {
+            const int ITER = 100;
+            const int MAX_N = 10;
+            
             using (var qsim = new QuantumSimulator())
             {
-                var temp = DJ_balanced.Run(qsim, 2).Result;
-                for (int i=1; i<10; i++)
+                var setup = DJ_balanced.Run(qsim, 2).Result;
+                long[] times = new long[MAX_N];
+
+                for (int n=1; n<=MAX_N; n++)
                 {
-                    var watch = System.Diagnostics.Stopwatch.StartNew();
-                    var r = DJ_balanced.Run(qsim, i).Result;
-                    watch.Stop();
-                    long time = watch.ElapsedTicks;
-                    Console.WriteLine($"Elapsed: {time}");
+                    long[] tempTimes = new long[ITER];
+                    for (int i=0; i < ITER; i++)
+                    {
+                        var watch = System.Diagnostics.Stopwatch.StartNew();
+                        var r = DJ_balanced.Run(qsim, n).Result;
+                        watch.Stop();
+                        tempTimes[i] = watch.ElapsedTicks;
+                    }
+                    times[n-1] = (long)tempTimes.Average();
+                    Console.WriteLine($"clock cycles for n={n}: {times[n-1]}");
                 }
+                Console.WriteLine("["+string.Join(", ", times)+"]");
             }
         }
     }
