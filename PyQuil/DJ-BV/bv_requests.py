ee0c4acffd547d66081382c42bbfbc6bc3a0a3c1
Zach Harris <zharris505@gmail.com>
2019-06-10 22:45:30 -0700

    Adding previous version of BV for housekeeping

--- PyQuil/DJ-BV/bv_requests.py
+++ PyQuil/DJ-BV/bv_requests.py
@@ -11,7 +11,7 @@ import time
 
 class Bernstein_Vazirani():
 
-    def __init__(self, n):
+    def __init__(self, n, map=None):
         """
         Initialize the class with a particular n
         :param n: the number of bits to use in the circuit
@@ -19,6 +19,19 @@ class Bernstein_Vazirani():
         self.p = Program()
         self.ro = self.p.declare('ro', 'BIT', n)
         self.n = n
+        # build a map of the aspen bits:
+        self.map = map
+
+    def from_map(self, i):
+        """
+        Returns a mapping to the qubits if one exists - otherwise just return identity
+        :param i: the qubit index of the map you want to map to
+        :return: the new qubit
+        """
+        if self.map is None:
+            return i
+        else:
+            return self.map[i]
 
     def build_circuit(self, a, b):
         """
@@ -39,7 +52,7 @@ class Bernstein_Vazirani():
         Initialize the first qubits to 0 and the last (helper) qubit to 1
         :return: program state after this operation
         """
-        self.p += Program([I(i) for i in range(self.n)] + [X(self.n)])
+        self.p += Program([I(self.from_map(i)) for i in range(self.n)] + [X(self.from_map(self.n))])
         return self.p
 
     def left_hadamards(self):
@@ -47,7 +60,7 @@ class Bernstein_Vazirani():
         Add a Hadamard gate to every qubit (corresponds to the left-hand-side hadamards in BV)
         :return: program state after this operation
         """
-        self.p += Program([H(i) for i in range(self.n + 1)])
+        self.p += Program([H(self.from_map(i)) for i in range(self.n + 1)])
         return self.p
 
     def insert_Uf(self, a=2, b=2):
@@ -60,16 +73,16 @@ class Bernstein_Vazirani():
 
         # if b is not even, then we need to flip the helper qubit (the last one)
         if b % 2 != 0:
-            self.p += Program(X(self.n))
+            self.p += Program(X(self.from_map(self.n)))
 
         # first convert a into a bitstring for easy use
         a = self.int_to_bits(a, self.n)
-        print('Expected:', a)
+        print('# Expected:', a)
 
         for i, _a in enumerate(a):
             # wherever _a is 1, we insert a CNOT into the system targeting the helper bit
             if _a == 1:
-                self.p += Program(CNOT(i, self.n))
+                self.p += Program(CNOT(self.from_map(i), self.from_map(self.n)))
 
         # return the program, which now has U_f inserted
         return self.p
@@ -79,7 +92,7 @@ class Bernstein_Vazirani():
         Add a Hadamard gate to every qubit but the helper (corresponds to the right-hand-side hadamards in BV)
         :return: program state after this operation
         """
-        self.p += Program([H(i) for i in range(self.n)])
+        self.p += Program([H(self.from_map(i)) for i in range(self.n)])
         return self.p
 
     def measure_ro(self):
@@ -87,7 +100,7 @@ class Bernstein_Vazirani():
         Measure every qubit but the last one
         :return: program state after this operation
         """
-        self.p += Program([MEASURE(i, self.ro[i]) for i in range(self.n)])
+        self.p += Program([MEASURE(self.from_map(i), self.ro[i]) for i in range(self.n)])
         return self.p
 
     @staticmethod
@@ -104,13 +117,17 @@ class Bernstein_Vazirani():
         return y
 
 
-def run_BV(n, a, b):
+def run_BV(n, a, b, pr=False, map=None):
     # setup the experiment
-    bv = Bernstein_Vazirani(n)
+    bv = Bernstein_Vazirani(n, map=map)
     p = bv.build_circuit(a, b)
 
+    if pr:
+        print(p)
+        return None
+
     # actually perform the measurement
-    qvm = get_qc('9q-square-qvm')
+    qvm = get_qc('Aspen-4-12Q-A')
     with local_qvm():
         # one way of measuring:
         t = time.time()
@@ -124,12 +141,17 @@ def run_BV(n, a, b):
     return result, return_time
 
 def main():
-    n = 5  # the number of bits in f: {0,1}^n → {0,1}
+    n = 6  # the number of bits in f: {0,1}^n → {0,1}
 
-    a = 25  # the a in f(x) = a*x + b
+    print('# --- Complex:')
+    a = (2**n)-1  # the a in f(x) = a*x + b
+    b = 0  # the b in f(x) = a*x + b
+    run_BV(n, a, b, pr=True, map=[0, 1, 2, 6, 7, 10, 11, 13, 14, 15, 16, 17])
+
+    print('# --- Simple:')
+    a = 0  # the a in f(x) = a*x + b
     b = 1  # the b in f(x) = a*x + b
-
-    run_BV(n, a, b)
+    run_BV(n, a, b, pr=True, map=[0, 1, 2, 6, 7, 10, 11, 13, 14, 15, 16, 17])
 
 
 if __name__ == '__main__':
